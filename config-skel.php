e61586a7bca9a5c906ca0ff88f47a26f5809bce1
Sun Zhigang <sunner@gmail.com>
2011-09-24 16:27:01 +0800

    jwc_manager框架

---
+++ config-skel.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbtype = 'mssql';
+$dbhost = '';
+$dbuser = '';
+$dbpass = '';
+$dbname = '';
+$debugdb = false;
--- locallib.php
+++ locallib.php
@@ -19,3 +19,40 @@ class setup_jwcid_form extends moodleform {
     }
 }
 
+/**
+ * 教务处功能封装
+ */
+class jwc_manager {
+    protected $extdb;
+
+    function __construct() {
+        global $CFG;
+        require_once($CFG->libdir.'/adodb/adodb.inc.php');
+        require_once('config.php');
+
+        // Connect to the external database (forcing new connection)
+        $extdb = ADONewConnection($dbtype);
+        if ($debugdb) {
+            $extdb->debug = true;
+            ob_start(); //start output buffer to allow later use of the page headers
+        }
+
+        $extdb->Connect($dbhost, $dbuser, $dbpass, $dbname, true);
+        $extdb->SetFetchMode(ADODB_FETCH_ASSOC);
+
+        $this->extdb = $extdb;
+    }
+}
+
+/**
+ * 返回jwc_manager类的实例
+ */
+function get_jwc_instance() {
+    static $jwc = null;
+    if ($jwc) {
+        return $jwc;
+    }
+
+    $jwc = new jwc_manager();
+    return $jwc;
+}
