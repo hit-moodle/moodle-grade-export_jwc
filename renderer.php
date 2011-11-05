a051901d29ec5b6157d828a6b3ff9cb7d131eb7a
Sun Zhigang <sunner@gmail.com>
2011-11-05 15:36:04 +0800

    新窗口打开教务处

--- renderer.php
+++ renderer.php
@@ -91,7 +91,7 @@ class gradeexport_jwc_renderer extends plugin_renderer_base {
     }
 
     public function success() {
-        $link = html_writer::link('http://xscj.hit.edu.cn/hitjwgl/teacher/log.asp', '登录教务处');
+        $link = html_writer::link('http://xscj.hit.edu.cn/hitjwgl/teacher/log.asp', '登录教务处', array('target' => '_blank'));
         return '成绩上传成功。请'.$link.'检查、确认和提交。';
     }
 }
